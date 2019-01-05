abe66941ccfface9c7813636c8c1060a37963335
arturolejnik95 <arturolejnik95@gmail.com>
2019-01-05 03:24:47 +0100

    Zmiany - klient + wygląd

--- Wisielec/gamewindow.cpp
+++ Wisielec/gamewindow.cpp
@@ -1,14 +1,41 @@
 #include "gamewindow.h"
 #include "ui_gamewindow.h"
 
-GameWindow::GameWindow(QWidget *parent) :
+#include <QMessageBox>
+
+using namespace std;
+
+GameWindow::GameWindow(QString host, int port, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::GameWindow)
 {
     ui->setupUi(this);
+    this->setHost(host);
+    this->setPort(port);
 }
 
 GameWindow::~GameWindow()
 {
     delete ui;
+}
+
+void GameWindow::connectToServer(){
+    sock = new QTcpSocket(this);
+    sock->connectToHost(ServerHost, ServerPort);
+
+    connect(sock, &QTcpSocket::connected, this, &GameWindow::socketConnected);
+
+    connTimeoutTimer = new QTimer(this);
+    connTimeoutTimer->setSingleShot(true);
+    connect(connTimeoutTimer, &QTimer::timeout, [&]{
+        connTimeoutTimer->deleteLater();
+        QMessageBox::critical(this, "Error", "Connect timed out");
+    });
+    connTimeoutTimer->start(3000);
+
+}
+
+void GameWindow::socketConnected() {
+    connTimeoutTimer->stop();
+    connTimeoutTimer->deleteLater();
 }
--- Wisielec/gamewindow.h
+++ Wisielec/gamewindow.h
@@ -2,6 +2,8 @@
 #define GAMEWINDOW_H
 
 #include <QWidget>
+#include <QTcpSocket>
+#include <QTimer>
 
 namespace Ui {
 class GameWindow;
@@ -12,11 +14,25 @@ class GameWindow : public QWidget
     Q_OBJECT
 
 public:
-    explicit GameWindow(QWidget *parent = nullptr);
+    explicit GameWindow(QString host, int port, QWidget *parent = nullptr);
     ~GameWindow();
+
+    void connectToServer();
+
+    void setHost(QString h) {this->ServerHost = h;}
+    void setPort(quint16 p) {this->ServerPort = p;}
+
+protected:
+    QTimer * connTimeoutTimer;
+    QTcpSocket * sock;
+
+    void socketConnected();
+
 
 private:
     Ui::GameWindow *ui;
+    QString ServerHost;
+    quint16 ServerPort;
 };
 
 #endif // GAMEWINDOW_H
