422d0c28952aa71e3f96e316c6108b0c114684c9
arturolejnik95 <arturolejnik95@gmail.com>
2019-01-19 04:05:18 +0100

    Uporządkowanie funkcji w playerze

--- Wisielec/player.h
+++ Wisielec/player.h
@@ -9,27 +9,34 @@ class Player {
 public:
     Player(int p, string n, int nr, int r);
     ~Player();
-    int getFd() {return playerFd;}
-    int getNumber() {return number;}
-    void setNumber(int n) {number = n;}
-    void setName(string n) {name = n;}
-    string getName() {return name;}
-    void hangPoint(int round);
+
     void connect() {connected = true;}
     void disconnect() {connected = false;}
+
+    int getFd() {return playerFd;}
+    int getNumber() {return number;}
+    string getName() {return name;}
+    int getPoints() {return points;}
     int getLives(int round) {return live[round-1];}
+
+    void addPoints(int p);
+    void setName(string n) {name = n;}
+    void setNumber(int n) {number = n;}
+
     bool hasLives() {return !endRound;}
     bool isConnected() {return connected;}
+
+    void hangPoint(int round);
     void newRound(int r);
-    void addPoints(int p);
-    int getPoints() {return points;}
-    bool operator > (Player* p1);
+
 private:
     int playerFd;
     int number;
     string name;
+
     bool connected;
     bool endRound;
+
     int live[5];
     int points;
     int round;
